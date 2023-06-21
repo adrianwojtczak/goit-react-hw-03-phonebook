23ec5c401bdf7a3e3dafdf98ce187073e57795c8
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-06-21 13:25:58 +0200

    Add: Save to Local Storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   addContact = (name, number) => {
     const { contacts } = this.state;
 
